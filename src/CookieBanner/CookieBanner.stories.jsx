b3bbf57ea517ffbd5e85518e4718da6683897277
AlasdairSwan <aswan@edx.org>
2018-04-05 10:49:49 -0400

    Outputting CSS and Sass files

--- src/CookieBanner/CookieBanner.stories.jsx
+++ src/CookieBanner/CookieBanner.stories.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import CookieBanner from './index';
-import styles from './CookieBanner.scss';
+import styles from './_cookie-banner.scss';
 
 storiesOf('Cookie Banner', CookieBanner)
   .add('basic usage', () => (<CookieBanner />));
