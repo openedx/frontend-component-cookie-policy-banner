563ebe49b16d2a93112530260bebb47a335e9885
Sharon Wang <sharon.ran.wang@gmail.com>
2020-12-03 15:14:18 -0500

    style updates

--- src/CookiePolicyBanner/index.jsx
+++ src/CookiePolicyBanner/index.jsx
@@ -29,6 +29,10 @@ class CookieBanner extends Component {
     this.toggleDisplay(!hasViewedCookieBanner());
   }
 
+  componentDidUpdate() {
+    document.querySelectorAll('.edx-cookie-banner .btn')[0].blur();
+  }
+
   onClose(event) {
     this.setState({ open: false }, () => {
       createHasViewedCookieBanner();
