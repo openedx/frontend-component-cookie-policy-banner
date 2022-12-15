81f6627343c44c33959b3b11b6c757546522c6f0
Jesse Day <jesse.day@adaptagency.com>
2022-12-15 12:07:42 -0500

    fix: update tests to pass.

--- src/utilties.test.js
+++ src/utilties.test.js
@@ -53,7 +53,7 @@ describe('utilities', () => {
     it('localhost cookie creation data', () => {
       jsdom.reconfigure({ url: `http://${LOCALHOST}:8080/` });
       const expected = {
-        cookieName: 'localhost-edx-cookie-policy-viewed',
+        cookieName: 'localhost-edx-updated-cookie-policy-viewed',
         domain: 'localhost',
         path: '/',
         maxAge: 2147483647,
@@ -65,7 +65,7 @@ describe('utilities', () => {
       const stageEnvironment = STAGE_ENVIRONMENTS.STAGE;
       jsdom.reconfigure({ url: `https://www.${stageEnvironment.baseURL}` });
       const expected = {
-        cookieName: 'stage-edx-cookie-policy-viewed',
+        cookieName: 'stage-edx-updated-cookie-policy-viewed',
         domain: '.stage.edx.org',
         path: '/',
         maxAge: 2147483647,
@@ -76,7 +76,7 @@ describe('utilities', () => {
     it('prod cookie creation data', () => {
       jsdom.reconfigure({ url: 'https://www.edx.org/' });
       const expected = {
-        cookieName: 'prod-edx-cookie-policy-viewed',
+        cookieName: 'prod-edx-updated-cookie-policy-viewed',
         domain: '.edx.org',
         path: '/',
         maxAge: 2147483647,
