17ecc140510120bc6c53f8ea3befcd54bbdfc66d
Jesse Day <jesse.day@adaptagency.com>
2022-12-16 14:23:05 -0500

    fix: linting issues.

--- src/CookiePolicyBanner/CookiePolicyBanner.stories.jsx
+++ src/CookiePolicyBanner/CookiePolicyBanner.stories.jsx
@@ -6,8 +6,8 @@ import { SPANISH_IETF_TAG, ENGLISH_IETF_TAG } from '../constants';
 import './_storybook-styles.scss';
 
 const policyText = {
-  [SPANISH_IETF_TAG]: `Hemos actualizado nuestra <a href="https://edx.org/es/edx-privacy-policy" class="policy-link" target = "_blank">Política de Privacidad</a> para mejor reflejar cómo coleccionamos, usamos y compartimos sus datos.`,
-  [ENGLISH_IETF_TAG]: `We've updated our <a href="https://edx.org/edx-privacy-policy" class="policy-link" target = "_blank">Privacy Policy</a> to better reflect how we collect, use and share your data.`,
+  [SPANISH_IETF_TAG]: 'Hemos actualizado nuestra <a href="https://edx.org/es/edx-privacy-policy" class="policy-link" target = "_blank">Política de Privacidad</a> para mejor reflejar cómo coleccionamos, usamos y compartimos sus datos.',
+  [ENGLISH_IETF_TAG]: 'We\'ve updated our <a href="https://edx.org/edx-privacy-policy" class="policy-link" target = "_blank">Privacy Policy</a> to better reflect how we collect, use and share your data.',
 };
 storiesOf('Cookie Policy Banner', CookiePolicyBanner)
   .add('basic usage', () => (<CookiePolicyBanner />))
