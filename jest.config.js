45ecb618430c49c3010412d04ac129afa51b9e61
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2023-03-22 17:47:40 +0500

    refactor: move jest configs to seperate jest-config file

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom-global',
+  testURL: 'http://localhost/',
+  setupFiles: [
+    './src/setupTest.js',
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!@edx)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/**.{js,jsx}',
+    '!src/**/*.stories.jsx',
+    '!src/setupTest.js',
+  ],
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileTransformer.js',
+  },
+};
